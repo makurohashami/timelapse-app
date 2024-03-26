38cef5eeb4a40ae4d514ddec579b8aa85286f7b1
Yaroslav Kotyk <makurohashami@gmail.com>
2024-03-26 10:25:31 +0200

    removed Spring

--- src/main/groovy/com/kotyk/timelapse/TimelapseApplication.groovy
+++ src/main/groovy/com/kotyk/timelapse/TimelapseApplication.groovy
@@ -4,23 +4,13 @@ import com.kotyk.timelapse.configuration.GlobalSettings
 import com.kotyk.timelapse.service.VideoService
 import com.kotyk.timelapse.service.WebCamService
 import com.kotyk.timelapse.util.FileUtil
-import groovy.util.logging.Slf4j
-import org.springframework.boot.CommandLineRunner
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@Slf4j
-@SpringBootApplication
-class TimelapseApplication implements CommandLineRunner {
+class TimelapseApplication {
 
     static void main(String[] args) {
-        SpringApplication.run(TimelapseApplication, args)
-    }
-
-    @Override
-    void run(String... args) throws Exception {
         FileUtil.createFolders(GlobalSettings.pathToPhotos)
         WebCamService.takePhotosAsync(GlobalSettings.pathToPhotos)
         VideoService.makeVideoFromPhotos(GlobalSettings.pathToPhotos, GlobalSettings.pathToTimelapse)
     }
+
 }
